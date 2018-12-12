4c9ebe77ba4fe0bb1f993fdaf77423aded3cd8a4
sqp0627 <1551936315@qq.com>
2018-12-12 10:21:22 +0800

    引入immutable、redux-immutable管理state

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -30,7 +30,8 @@ const Header = (props) => {
 
 const mapStateToProps = (state) => {
   return {
-    focused: state.header.focused
+    // focused: state.get('header').get('focused') // 跟下面的写法等价
+    focused: state.getIn(['header', 'focused'])
   }
 };
 const mapDispatchToProps = (dispatch) => {
--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,17 +1,16 @@
 import * as actionTypes from './actionTypes'
-const defaultState = {
+import {fromJS} from "immutable";
+
+const defaultState = fromJS({
   focused: false
-};
+});
 
 export default (state = defaultState, action) => {
   if (action.type === actionTypes.SEARCH_FOCUS) {
-    return {
-      focused: true
-    }
+    // immutable的set方法会结合之前的immutable对象的值和设置的值，返回一个新的对象
+    return state.set('focused', true)
   } else if (action.type === actionTypes.SEARCH_BLUR) {
-    return {
-      focused: false
-    }
+    return state.set('focused', false)
   }
   return state;
 }
